f5a199c38f58a1f3f00b4bc06d9a7f9fcdca2f2d
AR-DEV-1 <mail.ardev1@protonmail.com>
2025-01-14 12:29:53 +0500

    Create stdint.h for bootloader stage 2

---
+++ sys/include/stdint.h
@@ -0,0 +1,19 @@
+// For Bootloader Stage 2
+// First Commit in a while, I know
+// Version 1
+// Author = AR-DEV-1
+// This is licensed under the GPL 3.0
+// STDINT.H
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+
+#endif
