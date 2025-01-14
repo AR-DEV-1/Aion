fb44d19387deccfad64dd63665cfa998a61a7cce
AR-DEV-1 <mail.ardev1@protonmail.com>
2025-01-14 12:31:52 +0500

    Create stddef.h

---
+++ sys/include/stddef.h
@@ -0,0 +1,12 @@
+// Defination file for Common Macros, Will be updated later
+// Version 1
+// Author = AR-DEV-1
+// This file is licensed under GPL 3.0
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+#define NULL ((void *)0)
+
+typedef unsigned int size_t;
+
+#endif
